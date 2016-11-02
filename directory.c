109201e44626237f0d332b2b282bfde822e9b4db
Jackson Morgan <jmorgan@stuy.edu>
2016-11-02 18:08:57 -0400

    I am a moron

--- directory.c
+++ directory.c
@@ -8,64 +8,22 @@
 #include <dirent.h>
 #include <sys/types.h>
 
-typedef struct files{ 
-  char *name;
-  int binary;
-  struct files *next;
-} files;
 
 
 
 int main(){
-  struct files *temp =  (struct files *) malloc(sizeof(struct files));
-  struct files *sec = temp;
-  struct files *bin = temp;
-  struct files *bin1 = temp;
   DIR *dir = opendir(".");
   struct dirent *store = readdir(dir);
-  int i = 1;
 
-
-
-
+  printf("Directories:\n");
+  
   while(store!= NULL){
-    temp->name = store->d_name;
-    printf("%s\n", temp->name);
-    //str = readdir(dir)->d_name;
-    temp->next = (struct files *) malloc(sizeof(struct files));
-    temp = temp->next;
+    if(store->d_type == 4){
+      printf("%s\n", store->d_name);
+    }
     store = readdir(dir);
   }
   closedir(dir);
-
-  dir =opendir(".");
-  store = readdir(dir);
-
-   while(store!= NULL){
-     if(store->d_type == DT_DIR){
-       sec->binary == 1;
-     } 
-     printf("%d\n", store->d_type);
-     //str = readdir(dir)->d_name;
-     sec = sec->next;
-     store = readdir(dir);
-   }
-   closedir(dir);
-
-
-
-
-
-
   
-  while ( bin ) {
-    bin = bin->next;
-    free(bin1);
-    bin1 = bin;    
-  }
-  bin = NULL;
-  printf("tjat\n");
-    //close 
-  return 0;
-
+  
 }
